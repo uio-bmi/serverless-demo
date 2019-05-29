37e03b3348230ce1b47c9daa026c4d78bf4d8287
Dmytro Titov <dmytro.titov@gmail.com>
2019-05-29 13:25:50 +0200

    Fix test

--- src/test/kotlin/no/uio/ifi/bio/SmithWatermanTest.kt
+++ src/test/kotlin/no/uio/ifi/bio/SmithWatermanTest.kt
@@ -3,22 +3,19 @@ package no.uio.ifi.bio
 import org.junit.Test
 import kotlin.test.assertEquals
 
-val EXPECTED_RESULT = """
-CLUSTAL W MSA from BioJava
-
-P69905    LSPADKTNVKAAWGKVGAHAGEYGAEALERMFLSFPTTKTYFPHF-DLS-----HGSAQV  56
-          |.| .|. | | ||||  .  | | ||| |. . .| |. .|  | |||      |. .|
-P68871    LTPEEKSAVTALWGKV--NVDEVGGEALGRLLVVYPWTQRFFESFGDLSTPDAVMGNPKV  61
-
-P69905    KGHGKKVADALTNAVAHVDDMPNALSALSDLHAHKLRVDPVNFKLLSHCLLVTLAAHLPA 116
-          | |||||  | .. .||.|..    . ||.||  || ||| ||.|| . |.  || |
-P68871    KAHGKKVLGAFSDGLAHLDNLKGTFATLSELHCDKLHVDPENFRLLGNVLVCVLAHHFGK 121
-
-P69905    EFTPAVHASLDKFLASVSTVLTSKY 141
-          |||| |.|.  | .| |.  |  ||
-P68871    EFTPPVQAAYQKVVAGVANALAHKY 146
-
-""".trimIndent()
+const val EXPECTED_RESULT = "CLUSTAL W MSA from BioJava\n" +
+        "\n" +
+        "P69905    LSPADKTNVKAAWGKVGAHAGEYGAEALERMFLSFPTTKTYFPHF-DLS-----HGSAQV  56\n" +
+        "          |.| .|. | | ||||  .  | | ||| |. . .| |. .|  | |||      |. .|\n" +
+        "P68871    LTPEEKSAVTALWGKV--NVDEVGGEALGRLLVVYPWTQRFFESFGDLSTPDAVMGNPKV  61\n" +
+        "\n" +
+        "P69905    KGHGKKVADALTNAVAHVDDMPNALSALSDLHAHKLRVDPVNFKLLSHCLLVTLAAHLPA 116\n" +
+        "          | |||||  | .. .||.|..    . ||.||  || ||| ||.|| . |.  || |   \n" +
+        "P68871    KAHGKKVLGAFSDGLAHLDNLKGTFATLSELHCDKLHVDPENFRLLGNVLVCVLAHHFGK 121\n" +
+        "\n" +
+        "P69905    EFTPAVHASLDKFLASVSTVLTSKY 141\n" +
+        "          |||| |.|.  | .| |.  |  ||\n" +
+        "P68871    EFTPPVQAAYQKVVAGVANALAHKY 146\n"
 
 
 class SmithWatermanTest {
